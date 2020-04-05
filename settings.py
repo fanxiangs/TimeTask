82d45286bfb039e6125555b564645c7ca93188c9
fanxiangs <1378522044@qq.com>
2020-04-05 20:53:02 +0800

    配置ignore

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@
 @time:2020/04/05
 """
 ########邮件配置###################
-username_send = "fanxiangs@163.com"
-password = "VUDNOIREXXWIXFHP"
-username_recv = 'qianqiantj@qq.com'  # 收件人，多个收件人用逗号隔开
+username_send = "***"
+password = "***"
+username_recv = "***"  # 收件人，多个收件人用逗号隔开
